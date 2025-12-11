7ff7f8f76b2293e9909811bcef3bf9fe1f9ba5f2
hhhmb763-collab <hhhmb763@gmail.com>
2025-12-11 20:13:03 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import pool from '../db/init.js';
-import ejs from 'ejs';
 import fs from 'fs';
 import path from 'path';
+import ejs from 'ejs';
 
 const app = express();
 
@@ -10,17 +10,13 @@ app.get('/', async (req, res) => {
     const query = req.query.q;
     let sql = 'SELECT * FROM resources ORDER BY sort_order ASC, created_at DESC';
     let values = [];
-
     if (query) {
         sql = 'SELECT * FROM resources WHERE name ILIKE $1 OR tags ILIKE $1 OR description ILIKE $1 ORDER BY sort_order ASC, created_at DESC';
         values = [`%${query}%`];
     }
-
     const { rows } = await pool.query(sql, values);
-
     const template = fs.readFileSync(path.resolve('views/index.ejs'), 'utf-8');
-    const html = ejs.render(template, { resources: rows, search_query: query });
-    res.send(html);
+    res.send(ejs.render(template, { resources: rows, search_query: query }));
 });
 
 export default app;
